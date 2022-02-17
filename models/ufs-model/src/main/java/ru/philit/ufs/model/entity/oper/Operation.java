373d031974710844083624116ce09f7499a49b92
Aleksey-Servetnyk <73210231+Aleksey-Servetnyk@users.noreply.github.com>
2022-02-17 19:14:26 +0200

    the maven plugin to generate transport entities from the specified xsd file added and improving the Operation entity located in ufs-model so that it can participate in the integration continued

--- models/ufs-model/src/main/java/ru/philit/ufs/model/entity/oper/Operation.java
+++ models/ufs-model/src/main/java/ru/philit/ufs/model/entity/oper/Operation.java
@@ -1,12 +1,16 @@
 package ru.philit.ufs.model.entity.oper;
 
+import java.math.BigDecimal;
 import java.util.Date;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NonNull;
 import lombok.Setter;
 import lombok.ToString;
 import ru.philit.ufs.model.entity.common.ExternalEntity;
 import ru.philit.ufs.model.entity.common.OperationTypeCode;
+import ru.philit.ufs.model.entity.esb.as_fs.OpStatusType;
+import ru.philit.ufs.model.entity.esb.as_fs.OperTypeLabel;
 
 /**
  * Сущность операции.
@@ -24,5 +28,41 @@ public class Operation extends ExternalEntity {
   private Date createdDate;
   private Date committedDate;
   private String rollbackReason;
+
+  private String operationId;
+
+  //region From SrvCreateOperationRqMessage
+  @NonNull
+  private OperTypeLabel operationType;
+
+  @NonNull
+  private String workPlaceUId;
+
+  private String operatorId;
+  //endregion
+
+  //region From SrvUpdOperationRqMessage
+  protected String cashOrderId;
+  protected String operationNum;
+  protected OpStatusType operationStatus;
+  protected String repId;
+  protected String senderAccountTypeId;
+  protected String senderAccountCurrencyType;
+  protected String senderBank;
+  protected String senderBankBIC;
+
+  @NonNull
+  protected String senderAccountId;
+
+  @NonNull
+  protected BigDecimal amount;
+
+  protected String recipientAccountTypeId;
+  protected String recipientAccountCurrencyType;
+  protected String recipientBank;
+  protected String recipientBankBIC;
+  protected String recipientAccountId;
+  protected String currencyType;
+  //endregion
 
 }
